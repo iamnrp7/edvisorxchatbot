ec99e412988d6d08448ab2a8092f90516066594c
Nihar Prajapati <143262287+iamnrp7@users.noreply.github.com>
2024-09-06 08:25:49 +0530

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -89,15 +89,6 @@ export function ChatPanel({
                       <div className="text-red-600">
                         You have reached your message limit! Please try again
                         later, or{' '}
-                        <a
-                          className="underline"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          href="https://vercel.com/templates/next.js/gemini-ai-chatbot"
-                        >
-                          deploy your own version
-                        </a>
-                        .
                       </div>
                     )
                   }
