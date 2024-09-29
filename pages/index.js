1ea113b35be7de876e694fd443d13a505ad73f6b
Nihar Prajapati <143262287+iamnrp7@users.noreply.github.com>
2024-09-29 12:42:23 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+
+import GiftSuggestionForm from '../components/GiftSuggestionForm';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Gift Suggestion Bot</h1>
+      <GiftSuggestionForm />
+    </div>
+  );
+}
